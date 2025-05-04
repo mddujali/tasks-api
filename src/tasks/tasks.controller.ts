60d977197ef6657d9b4dd036e37db2537976d520
Mark Dujali <mddujali@gmail.com>
2025-05-04 22:44:37 +0800

    Optimize code

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,7 +6,9 @@ import {
   Param,
   Post,
   Put,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -15,30 +17,40 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  async getTasks(): Promise<Task[]> {
-    return await this.tasksService.getTasks();
+  async getTasks(@Res() res: Response): Promise<Response> {
+    return res.json(await this.tasksService.getTasks());
   }
 
   @Get(':id')
-  async getTask(@Param('id') id: string): Promise<any> {
-    return await this.tasksService.getTask(id);
+  async getTask(
+    @Param('id') id: string,
+    @Res() res: Response,
+  ): Promise<Response> {
+    return res.json(await this.tasksService.getTask(id));
   }
 
   @Post()
-  async createTask(@Body() task: Omit<Task, 'id'>): Promise<Task> {
-    return await this.tasksService.createTask(task);
+  async createTask(
+    @Body() task: Omit<Task, 'id'>,
+    @Res() res: Response,
+  ): Promise<Response> {
+    return res.json(await this.tasksService.createTask(task));
   }
 
   @Put(':id')
   async updateTask(
     @Param('id') id: string,
     @Body() task: Omit<Task, 'id'>,
-  ): Promise<Task> {
-    return await this.tasksService.updateTask(id, task);
+    @Res() res: Response,
+  ): Promise<Response> {
+    return res.json(await this.tasksService.updateTask(id, task));
   }
 
   @Delete(':id')
-  async deleteTask(@Param('id') id: string): Promise<void> {
-    return await this.tasksService.deleteTask(id);
+  async deleteTask(
+    @Param('id') id: string,
+    @Res() res: Response,
+  ): Promise<Response> {
+    return res.json(await this.tasksService.deleteTask(id));
   }
 }
